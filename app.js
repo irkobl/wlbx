ed357f1c8cbfef6a6a6fc4ea40cbc130b4bd51b2
Your Name <you@example.com>
2022-09-10 12:26:07 +0800

    commit

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const createError = require ('http-errors');
 const express = require ('express');
 const path = require ('path');
 
+
 const indexRouter = require('./routes/index');
 const formsRouter = require('./routes/forms');
 const registerRouter = require('./routes/register');
@@ -13,14 +14,19 @@ const app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 app.use('/css', express.static(__dirname + '/node_modules/bootstrap/dist/css'));
+app.use('/js', express.static(__dirname + '/node_modules/bootstrap/dist/js'));
+app.use('/public', express.static('public'))
+app.disable('etag');
 app.use(express.urlencoded({ extended: false }));
 app.use(fileUpload());
+
+
 
 app.use('/', indexRouter, formsRouter, registerRouter);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  return next(createError(404));
-});
+// app.use(function(req, res, next) {
+//   return next(createError(404));
+// });
 
 module.exports = app;
--- controllers/formsIndex.js
+++ controllers/formsIndex.js
@@ -1,25 +1,62 @@
 const connectDB = require ('../database/models/');
+const iconv = require ('iconv-lite');
+const stream = require ('stream');
 
+exports.forms = async (req, res, next) => {
 
-exports.forms = (res, req, next) => {
-    req.render('forms', {title: 'Forms', test: 'test', login: res.user.login});
+    //await connectDB.blog.destroy({ where:{}, truncate: true }); // dev чистим таблицу от мусора
+    // await connectDB.blog.update({name: "tempora rem veritatis voluptas quo dolores vero"}, {
+    //     where: {id: 41}
+    // });
+
+    const objBlogs = await connectDB.blog.findAll({ where: {userID: req.user.id} });
+    
+    console.log(objBlogs.length, typeof objBlogs);
+            
+    const jsonBlogs = new Object();
+    objBlogs.forEach((dt) => {        
+        jsonBlogs[dt.dataValues.id] = dt.dataValues; 
+    });
+
+    let string = JSON.stringify(jsonBlogs, function (key, value) {
+        if (key == "message") {
+            return value.replace(/\r\n\r\n/g, '<br>').replace(/\r\n/g, '&emsp;');
+          } else {
+            return value;
+          }
+    });
+
+    const readBlogs = stream.Readable.from(string, {encoding: 'utf8'});     
+
+    readBlogs.on('data', function(chunk) {                
+        res.render('forms', {title: 'Forms', login: req.user.login, blogs: chunk});
+    });
+    readBlogs.on('end', function() {    
+        res.end();
+    });
+    res.on ('close', function () {    
+        readBlogs.destroy();    
+    });    
+    
 };
 
-exports.blog = async (res, req, next) => {
-    let {input, text} = res.body;
-    let obj = {...res.files};
+exports.blog = async (req, res, next) => {
+    let {input, text} = req.body;    
+    let obj = {...req.files};
     let pathFile = '';    
-    if (Object.keys(obj).length !== 0) {
-        console.log()
-        pathFile = `${process.cwd()}/public/uploads/${obj.file.name}`; // /app/public/uploads/
+    if (Object.keys(obj).length !== 0) { 
+        iconv.skipDecodeWarning = true; //откючаем варнинги      
+        pathFile = `${process.cwd()}/public/uploads/${iconv.decode(obj.file.name, 'UTF-8').toString()}`; // проблема с кирилицей в именах файлов
         obj.file.mv(pathFile, (err) => {
             if (err) {
-                return req.status(500).json({message: `File is not downloaded ${error}`});
+                return res.status(500).json({message: `File is not downloaded ${error}`});
             }
         });
     }   
     
-    await connectDB.blog.create({name: input, message: text, media: pathFile, userID: res.user.id, });
-
-    return req.render('forms', {title: 'Forms', test: 'test', login: res.user.login});    
+    await connectDB.blog.create({name: input, message: text, media: pathFile, userID: req.user.id, });
+    
+   
+    return res.render('forms', {title: 'Forms', login: req.user.login});    
 };
+
